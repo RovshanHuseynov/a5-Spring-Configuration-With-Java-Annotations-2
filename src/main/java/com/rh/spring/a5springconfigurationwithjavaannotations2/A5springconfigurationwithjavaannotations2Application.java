66adbab985bd09d4414d8b1595aeef7e025e1200
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-20 11:26:46 +0400

    Initial commit

---
+++ src/main/java/com/rh/spring/a5springconfigurationwithjavaannotations2/A5springconfigurationwithjavaannotations2Application.java
@@ -0,0 +1,13 @@
+package com.rh.spring.a5springconfigurationwithjavaannotations2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A5springconfigurationwithjavaannotations2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A5springconfigurationwithjavaannotations2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/rh/spring/a5springconfigurationwithjavaannotations2/A5springconfigurationwithjavaannotations2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.spring.a5springconfigurationwithjavaannotations2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A5springconfigurationwithjavaannotations2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
